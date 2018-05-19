7dc45fa601f3fd0b89ca3c1fefd6a600ef7869e2
sayands <sayandsarkar.1997@gmail.com>
2018-05-19 14:57:46 +0300

    Added Restuarant Review Classifier Model using NLP

---
+++ Natural-Language-Processing/Restuarant_Reviews/Restuarant_Review_Classifier.py
@@ -0,0 +1,48 @@
+# Natural Langauge Processing for Restuarant Reviews
+
+# Importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning The texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag Of Words Model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values
+
+#Splitting the dataset into the Training Set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.20, random_state = 0)
+
+#Fitting Classifier To The Training Set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy' ,random_state = 0)
+classifier.fit(X_train,Y_train)
+
+#Predicting The Test Set Results
+y_pred = classifier.predict(X_test)
+
+#Making The Confusion Matrix and Classification Report
+from sklearn.metrics import classification_report,confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+cr = classification_report(Y_test, y_pred)
